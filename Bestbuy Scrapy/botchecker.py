7210de1d77b0648b97a5a94f72aa0a8a2ec9e414
jmedinamireles <jmedinamireles@users.noreply.github.com>
2021-01-13 16:28:14 -0600

    Update botchecker.py

--- Bestbuy Scrapy/botchecker.py
+++ Bestbuy Scrapy/botchecker.py
@@ -10,7 +10,8 @@ url = 'https://www.bestbuy.com/site/amd-ryzen-9-5900x-4th-gen-12-core-24-threads
 browser = webdriver.Chrome()
 
 webhook = DiscordWebhook(url='https://discord.com/api/webhooks/791708222326177793/_WwP0u2Yc8Qxi6LG5ZEFk_qQ89Ocsn9dkXibrOjAnpbq1fr8EuQELJPzcBnyMdBvfPiM',
-                                content='<@&794330080553599017> 5900x is available https://www.bestbuy.com/site/amd-ryzen-9-5900x-4th-gen-12-core-24-threads-unlocked-desktop-processor-without-cooler/6438942.p?skuId=6438942') 
+                                content='<@&794330080553599017> 5900x is available ' +
+                                'https://www.bestbuy.com/site/amd-ryzen-9-5900x-4th-gen-12-core-24-threads-unlocked-desktop-processor-without-cooler/6438942.p?skuId=6438942') 
 
 
 
@@ -25,9 +26,9 @@ def main():
         body = browser.find_element_by_xpath('/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[6]/div[1]/div/div/div/button')
         
 
-        if (body.text == "Add to Cart"):
+        if (body.text != "Add to Cart"):
             response = webhook.execute()
-            sleep(10)
+            sleep(5)
             
         sleep(randint(10,15))
         
